9e4bbd9ef38d57abda27baf9d4930f9ae3c9ee18
Ted Semashov <ted.semashov@gmail.com>
2023-03-27 15:05:26 +0200

    client side navigation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+import Link from 'next/link';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -11,7 +12,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read <Link href="/posts/first-post">this page!</Link>
         </h1>
 
         <p className={styles.description}>
@@ -113,3 +114,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home;

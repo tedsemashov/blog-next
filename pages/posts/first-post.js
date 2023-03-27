000f8262bd2dfd89aea15fa2ab67a48fda7c26f1
Ted Semashov <ted.semashov@gmail.com>
2023-03-27 17:32:29 +0200

    assets, metadata and css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,22 @@
-import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
 
 const FirstPost = () => {
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload" // tells Next.js to load this particular script lazily during browser idle time
+                onLoad={() =>
+                    console.log(`script loaded correctly, window.FB has been populated`)
+                } // is used to run any JavaScript code immediately after the script has finished loading
+            />
             <h1>First Post</h1>
-            <h2>
-                <Link href="/">Back to home</Link>
-            </h2>
-        </>
+        </Layout>
     );
 }
 
